debba7de4a0b6ec529d89669fea308f7bf6c9cd3
furqanalishah <syedfurqan@wanclouds.net>
2022-01-26 00:52:54 +0500

    add to_json() to Store and unique title contraint

--- models.py
+++ models.py
@@ -9,13 +9,14 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-    __table_args__ = (UniqueConstraint('email'), UniqueConstraint('user_name'),)
     id = Column(String(32), primary_key=True, index=True)
     user_name = Column(String(100), nullable=False)
     email = Column(String(50), nullable=False)
     hash_password = Column(String(512), nullable=False)
 
-    store = relationship("Store", backref="user", cascade="all, delete-orphan", lazy="dynamic")
+    store = relationship("Store", backref="user", cascade="all, delete-orphan", uselist=False)
+
+    __table_args__ = (UniqueConstraint('email'), UniqueConstraint('user_name'),)
 
     def __init__(self, user_name, email, hash_password):
         self.id = str(uuid.uuid4().hex)
@@ -32,7 +33,16 @@ class Store(Base):
 
     user_id = Column(String(32), ForeignKey("users.id"), nullable=False)
 
+    __table_args__ = (UniqueConstraint('title'),)
+
     def __init__(self, title, description=None):
         self.id = str(uuid.uuid4().hex)
         self.title = title
         self.description = description
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description
+        }
